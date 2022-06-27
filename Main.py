f826348a82dd8510f863869ec0f4cb89ea7fc019
karlito78 <107962821+karlito78@users.noreply.github.com>
2022-06-27 13:05:55 +0300

    init: test commit 27.06
    Signed-off-by: karlito78 <107962821+karlito78@users.noreply.github.com>

--- Main.py
+++ Main.py
@@ -1,25 +1,9 @@
-# Напиши функцію split_string, яка приймає рядок і повертає список рядків з двох символів.
-# Якщо рядок містить непарну кількість символів, тоді другий символ потрібно замінити на підкреслення ("_").
-
-# Приклад:
-#
-# split_string("abc") == ["ab", "c_"]
-# split_string("abcdef") == ["ab", "cd", "ef"]
-
-# string = "HelloWorld"
-# array = list(string)
-# print(array)
-
-def split_string(string: str) -> list:
-
-    array = list(string)
-    list_length = len(array)
-    if list_length % 2 == 1:
-        array.append("_")
+def quieter_please(mystring):
+    if mystring.isupper():
+        return mystring.lower()
     else:
-        pass
-    paired_letters = [array[i] + array[i + 1] for i in range(0, len(array) - 1, 2)]
+        return mystring
 
-    return paired_letters
 
-print(split_string("mamamam"))
+
+print(quieter_please("i Want to test"))
