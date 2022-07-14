2ef318faf4291dac64cfd784c75e8c94ccbbbabf
karlito78 <107962821+karlito78@users.noreply.github.com>
2022-07-14 21:11:15 +0300

    init: test commit 14.07
    Signed-off-by: karlito78 <107962821+karlito78@users.noreply.github.com>

--- Main.py
+++ Main.py
@@ -1,9 +1,23 @@
-
-for i in range(1, 10):
-    for j in range(10-i):
+for i in range(10):
+    # Print leading spaces
+    for j in range(1, 10-i):
+        print (" ",end=" ")
+    # Count up
+    for j in range(1,i+1):
+        print (j,end=" ")
+    # Count down
+    for j in range(i-1,0,-1):
+        print (j,end=" ")
+    # Next row
+    print()
+for i in range(1, 9):
+    for j in range(i):
         print(" ", end=" ")
-    for j in range(1, i+1):
+    for j in range(0, 9 - i):
+        print( j+1, end=" ")
+        # Count down
+    for j in range(8-i,0, -1):
         print(j, end=" ")
-    for j in range(2, i+1):
-        print(i - j+1, end=" ")
+        # Next row
+
     print()
