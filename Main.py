3543400d5b1b521a7f531cbf0bf6a819eaf8e729
karlito78 <107962821+karlito78@users.noreply.github.com>
2022-07-14 19:17:41 +0300

    init: test commit 14.07
    Signed-off-by: karlito78 <107962821+karlito78@users.noreply.github.com>

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
-def quieter_please(mystring):
-    if mystring.isupper():
-        return mystring.lower()
-    else:
-        return mystring
 
-
-
-print(quieter_please("i Want to test"))
+for i in range(1, 10):
+    for j in range(10-i):
+        print(" ", end=" ")
+    for j in range(1, i+1):
+        print(j, end=" ")
+    for j in range(2, i+1):
+        print(i - j+1, end=" ")
+    print()
